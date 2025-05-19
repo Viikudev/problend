97122e7aa09c37c9fcb6ae295f1e5d62a55268f2
Victor Samuel <victoress18@gmail.com>
2025-05-19 01:10:14 -0400

    feat: delete option for issues

--- app/components/IssueCard.tsx
+++ app/components/IssueCard.tsx
@@ -1,22 +1,40 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import axios from "axios";
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { IssueProps, areas } from "../types/issue";
 import { SignedIn, SignInButton, SignedOut } from "@clerk/nextjs";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/components/ui/alert-dialog";
+import { useUser } from "@clerk/nextjs";
 
 function IssueCard({
   id,
   area,
   title,
+  userId,
   createdAt,
   hasAnswer,
   status,
 }: IssueProps) {
   const [isButtonHovered, setIsButtonHovered] = useState(false);
   const date = new Date(createdAt).toLocaleDateString();
+
+  const { user } = useUser();
+  const router = useRouter();
 
   const areaLabel = areas[area];
   const areaColorMap: Record<string, string> = {
@@ -31,6 +49,10 @@ function IssueCard({
     graphicDesign: "text-indigo-700",
     economy: "text-lime-700",
     other: "text-zinc-700",
+  };
+
+  const handleDelete = async () => {
+    await axios.delete(`/api/issues/${id}`);
   };
 
   return (
@@ -68,31 +90,61 @@ function IssueCard({
       <div className="flex flex-col gap-10 py-2 px-2">
         <div className="flex justify-between items-end">
           <div className="text-xs">Created at {date}</div>
-          <SignedIn>
-            <Link href={`/issues/${id}`} className="flex gap-2">
-              <Button
-                variant="destructive"
-                className="hover:scale-105 cursor-pointer"
-              >
-                <Image
-                  src="/delete-icon.svg"
-                  alt="detele icon"
-                  width={25}
-                  height={25}
-                />
-              </Button>
-              <Button
-                variant="default"
-                className={`text-xs font-bold transition-all duration-200 ${
-                  isButtonHovered ? "scale-105" : "scale-100"
-                }`}
-                onMouseEnter={() => setIsButtonHovered(true)}
-                onMouseLeave={() => setIsButtonHovered(false)}
-              >
-                {status === "available" ? "Write Answer" : "See Answer"}
-              </Button>
-            </Link>
-          </SignedIn>
+          <div className="flex gap-2">
+            <SignedIn>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  {user && user.id === userId && (
+                    <Button
+                      variant="destructive"
+                      className="hover:scale-105 cursor-pointer"
+                    >
+                      <Image
+                        src="/delete-icon.svg"
+                        alt="detele icon"
+                        width={25}
+                        height={25}
+                      />
+                    </Button>
+                  )}
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you sure to delete this issue?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      The card will be permanenently removed and cant be
+                      recover.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel className="bg-secondary">
+                      Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={handleDelete}
+                      className="bg-destructive"
+                    >
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+              <Link href={`/issues/${id}`} className="flex gap-2">
+                <Button
+                  variant="default"
+                  className={`text-xs font-bold transition-all duration-200 ${
+                    isButtonHovered ? "scale-105" : "scale-100"
+                  }`}
+                  onMouseEnter={() => setIsButtonHovered(true)}
+                  onMouseLeave={() => setIsButtonHovered(false)}
+                >
+                  {status === "available" ? "Write Answer" : "See Answer"}
+                </Button>
+              </Link>
+            </SignedIn>
+          </div>
           <SignedOut>
             <SignInButton mode="modal">
               <Button
